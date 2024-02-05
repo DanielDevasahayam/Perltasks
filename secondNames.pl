4522502efca9dfcf48a91f2d8627da2ad0740b99
Daniel Devasahayam Arockia Jesuraj <ardaniel@athenahealth.com>
2024-02-05 13:26:56 +0530

    Modified SecondNames.pl

--- secondNames.pl
+++ secondNames.pl
@@ -1,44 +1,18 @@
-use Data::Dumper;
 
-my %map;
-sub storeNames {
-    print "Enter the name : ";
-    my $names = <STDIN>;
-    chomp $names;
+sub main() {
+    print "Enter the words using commas : ";
+    my $words = <STDIN>;
+    chomp $words;
+    my @arr = split(/,/,$words);
 
-    @words = split(/ /,$names);
-
-    print("last word : ", $words[$#words], "\n");
-
-    my $l = $map{$words[$#words]} // [];  
-    push(@$l, @words);  
-
-    $map{$words[$#words]} = $l;
-
-    my $mapLen = scalar(keys %map);
-    print "Length of the map : ", $mapLen, "\n";
-    foreach my $key ( keys %$map ) { 
-        print $key, " => ", $map->{$key},"\n";
-    }
-
- }
-
-sub main {
-    print "Enter the number of words you will enter : ";
-    my $n = <STDIN>;
-    chomp $n;
-
-    for (0 .. n) {
-        storeNames();
-    }
-
-    print "Enter the second name you wanna search : ";
+    print "Enter the name you wanna search : ";
     my $secondName = <STDIN>;
     chomp $secondName;
-
-    my $l = $map[$secondName];
-    print($#l + 1);
-   
+    for my $ele(@arr) {
+        my @newWords = split(/ /,$ele);
+        if ($newWords[$#newWords] eq $secondName) {
+            print $ele,"\n";
+        }
+    }
 }
-
 main();
