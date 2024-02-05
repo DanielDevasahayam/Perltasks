88d73cc96969fc2dfcf49a04ee86295fd683067a
Daniel Devasahayam Arockia Jesuraj <ardaniel@athenahealth.com>
2024-02-05 14:19:14 +0530

    Modified SecondNames.pl

--- secondNames.pl
+++ secondNames.pl
@@ -1,4 +1,5 @@
-
+=Write a perl script that could store names with multiple duplicate last names (for example, Salman Khan, Shah Rukh Khan, and Aamir Khan). The names must be got from the user through terminal. The script must allow the user to search names based on the last name (for example, Searching for khan should produce Salman Khan, Shah Rukh Khan, and Aamir Khan as results). The search must be case insensitive.
+=cut
 sub main() {
     print "Enter the words using commas : ";
     my $words = <STDIN>;
